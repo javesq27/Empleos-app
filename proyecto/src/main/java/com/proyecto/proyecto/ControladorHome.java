852f392b63bfe27220d1ddd3beefba7e995d3a3d
alexisarto <alexis.artopoulos@alumnos.pestalozzi.edu.ar>
2021-11-05 19:53:23 -0300

    ControladorSolicitudes, RepositorioPerfiles y correcion de shoulds de ultimo pull request con CodigoEstadoUsuario y CodigoPerfilUsuario

--- proyecto/src/main/java/com/proyecto/proyecto/ControladorHome.java
+++ proyecto/src/main/java/com/proyecto/proyecto/ControladorHome.java
@@ -39,7 +39,7 @@ public class ControladorHome {
     
         if(session.getAttribute("usuario") == null) {
             Usuario usuario = servicioUsuarios.buscarPorUsername(username);
-            usuario.setPassword(null);
+            servicioUsuarios.setContraseña(usuario, null);
             session.setAttribute("usuario", usuario);
  
         }
@@ -56,11 +56,11 @@ public class ControladorHome {
     @PostMapping("/signup")
     public String guardarRegistro(Usuario usuario, RedirectAttributes attributes) {
 
-        usuario.setEstatus(1); 
+        usuario.setEstatus(CodigoEstadoUsuario.HABILITADO);
 		usuario.setFechaRegistro(new Date()); 
 		
 		Perfil perfil = new Perfil();
-		perfil.setId(3); 
+        perfil.setId(CodigoPerfilUsuario.USUARIO);
 		usuario.agregar(perfil);
         
         servicioUsuarios.guardar(usuario);
